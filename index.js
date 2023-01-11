eef2e799d6f9ec74811425f17040502ed0dc2e21
KESARKAR, Abhijeet <abhijeet.kersarkar@auspost.com.au>
2023-01-11 17:42:03 +1100

    Format

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ const tobeprocessed = tobe.replace(new RegExp("<(/?)[-A-Za-z0-9]+:",'g'),'<$1').
 const schema = {};
 const options = {ignoreDoctype:true};
 
-const filterAndPrint = (filter, result)=>{
-    console.log('\n',filter.toUpperCase())
-    result.filter(r=>r.message.endsWith(filter)).map(a=>a.path).forEach(a=>console.log(a))
+const filterAndPrintNotPresent = (filter, result)=>{
+    console.log('\n>>>',filter.toUpperCase(), '\n');
+    result.filter(r=>r.message.endsWith(filter)).map(a=>a.path).forEach(a=>console.log(a));
+}
+
+const filterAndPrintDifferenceInValue = (filter, result) =>{
+    console.log('\n>>>',filter.toUpperCase(), '(LHS value   RHS value)','\n');
+    result.filter(a => a.resultType === filter).map(a => a.message.replace('field ', '').replace('has lhs value ', '\t').replace('and rhs value ', '\t')).forEach(a => console.log(a));
 }
 
 diffAsXml(asis, tobeprocessed, schema, options, (result) => {
-
-    filterAndPrint('not present in rhs', result)
-    filterAndPrint('not present in lhs', result)
-
-    console.log('\n','different lhs rhs')
-    result.filter(a=>a.resultType==='difference in element value').map(a=>a.message.replace('field ','').replace('has lhs value ', '\t').replace('and rhs value ', '\t')).forEach(a=>console.log(a))
+    filterAndPrintNotPresent('not present in rhs', result);
+    filterAndPrintNotPresent('not present in lhs', result);
+    filterAndPrintDifferenceInValue('difference in element value', result);
 });
 
